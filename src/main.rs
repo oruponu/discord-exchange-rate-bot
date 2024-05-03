9068cb4ecb9e494030a443cbeeeb04b6acc48382
oruponu <oruponu@outlook.com>
2024-05-03 20:55:11 +0900

    feat: 外国為替FX APIのJSON構造体を定義

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct TickerResponse {
+    status: i32,
+    data: Vec<Currency>,
+    responsetime: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Currency {
+    symbol: String,
+    ask: String,
+    bid: String,
+    timestamp: String,
+    status: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
