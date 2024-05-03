32a8be23d8206128ce596e7296dd6eecb15fedb0
oruponu <oruponu@outlook.com>
2024-05-03 23:55:15 +0900

    refactor: 不要なフィールドを削除

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,13 @@ struct Handler {
 
 #[derive(Deserialize, Debug)]
 struct TickerResponse {
-    status: i32,
     data: Vec<Currency>,
-    responsetime: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct Currency {
     symbol: String,
-    ask: String,
     bid: String,
-    timestamp: String,
-    status: String,
 }
 
 #[async_trait]
